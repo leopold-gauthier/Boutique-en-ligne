979f4fdc3b4d520a8f38557be883a11887fb8f17
leopold-gauthier <leopold.gauthier-de-porry@laplateforme.io>
2023-05-17 15:15:41 +0200

    add the system of the subcategory add and delete

--- hommes.php
+++ hommes.php
@@ -1,18 +1,25 @@
 <?php
-include_once('./include/nav_inc.php');
-include_once('./include/head_inc.php');
+include_once('./class/User.php');
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="./style/style.css">
+    <?php
+    include_once('./include/head_inc.php');
+    ?>
+    <link rel="stylesheet" href="./style/style.css">
     <title>Hommes</title>
 </head>
+
 <body>
+    <header>
+        <?php include_once('./include/nav_inc.php'); ?>
+    </header>
     <div id="container">
         <main>
             <div class="manbtn">
@@ -31,4 +38,5 @@ include_once('./include/head_inc.php');
         </footer>
     </div>
 </body>
+
 </html>
