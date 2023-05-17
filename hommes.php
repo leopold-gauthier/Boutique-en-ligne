9dfaf4b93e94a8b5d7e717c9dedc701ee47602cd
jaumaux julien <julien.jaumaux@laplateforme.io>
2023-05-17 11:28:24 +0200

    commit footer_inc.php

---
+++ hommes.php
@@ -0,0 +1,34 @@
+<?php
+include_once('./include/nav_inc.php');
+include_once('./include/head_inc.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="./style/style.css">
+    <title>Hommes</title>
+</head>
+<body>
+    
+    <main>
+        <div class="manbtn">
+            <h3>Hommes</h3>
+            <button type="button" class="btn btn-secondary">T-shirt</button>
+            <button type="button" class="btn btn-secondary">Pull</button>
+            <button type="button" class="btn btn-secondary">Short</button>
+            <button type="button" class="btn btn-secondary">Pantalon</button>
+            <button type="button" class="btn btn-light"><i class="fa-solid fa-filter"></i>Filtrer</button>
+        </div>
+        <div class="manpct_t"></div>
+        <div class="manpct_b"></div>
+    </main>
+    <footer>
+        <?php include_once('./include/footer_inc.php') ?>
+    </footer>
+    
+</body>
+</html>
---
+++ include/footer_inc.php
@@ -0,0 +1,34 @@
+<hr>
+<div id="footer">
+    <div id="f_block1">
+        <h6>Suivez nous !</h6>
+        <div id="icon_block1">
+            <i class="fa-brands fa-github"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-discord"></i>
+            <i class="fa-brands fa-facebook"></i>
+        </div>
+    </div>
+    <div class="border"></div>
+    <div id="f_block2">
+        <h6>Paiements sécurisés</h6>
+        <p>Paiement faciles et en plusieurs fois</p>
+        <div id="icon_block2">
+            <i class="fa-brands fa-cc-visa"></i>
+            <i class="fa-brands fa-cc-mastercard"></i>
+            <i class="fa-brands fa-google-pay"></i>
+            <i class="fa-brands fa-cc-apple-pay"></i>
+            <i class="fa-brands fa-cc-amex"></i>
+            <i class="fa-brands fa-paypal"></i>
+        </div>
+    </div>
+    <div class="border"></div>
+    <div id="f_block3">
+        <h6>Services clients</h6>
+        <div id="icon_block3">
+            <p>Retour facile &nbsp; <i class="fa-solid fa-arrows-rotate"></i></p>
+            <p>Click & Collect &nbsp; <i class="fa-solid fa-computer-mouse"></i></p>
+            <p>Livraison &nbsp; <i class="fa-solid fa-truck"></i></p>
+        </div>
+    </div>
+</div>
