560094354893c1109d3e0edfeb6691be3434a167
jaumaux julien <julien.jaumaux@laplateforme.io>
2023-05-23 13:00:10 +0200

    commit css 2

--- include/nav_inc.php
+++ include/nav_inc.php
@@ -12,7 +12,7 @@ $resultcategory = $category->fetchAll(PDO::FETCH_ASSOC);
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="me-3">LASAPPE</div>
+        <div id="logo" class="me-3">LASAPPE</div>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
