3ad0c7d679694eab70293fd74d648e37e847e48b
jaumaux julien <julien.jaumaux@laplateforme.io>
2023-05-25 12:32:51 +0200

    commit profil et edit profil

--- profil.php
+++ profil.php
@@ -20,8 +20,44 @@ include_once("./class/User.php")
     </header>
     <main>
         <div class="container">
-            <?php
-            var_dump($_SESSION);
+            <h3>Profil</h3>
+                <div class="profil_form">
+                        <form>
+                            <fieldset disabled>
+                                <div class="mb-3">
+                                <label for="disabledTextInput" class="form-label">Nom :</label>
+                                <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                                <div class="mb-3">
+                                <label for="disabledTextInput" class="form-label">Prénom :</label>
+                                <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                                <div class="mb-3">
+                                <label for="disabledTextInput" class="form-label">Email :</label>
+                                <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                                <div class="mb-3">
+                                <label for="disabledTextInput" class="form-label">Téléphone :</label>
+                                <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                                <div class="mb-3">
+                                <label for="disabledTextInput" class="form-label">Rue :</label>
+                                <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                                <div class="mb-3">
+                                <label for="disabledTextInput" class="form-label">Ville :</label>
+                                <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                                <div class="mb-3">
+                                <label for="disabledTextInput" class="form-label">Code postal :</label>
+                                <input type="text" id="disabledTextInput" class="form-control" placeholder="">
+                                </div>
+                            </fieldset>
+                            <a href="./edit.php"><button type="button" class="btn btn-primary"><i class="fa-solid fa-pen-to-square" style="color: #000000;"></i>Modifier votre profil</button></a>
+                    </form>
+                </div>
+            <!--                 zone à revoir              -->
+            <?php 
             if ($_SESSION == NULL) { ?>
                 <div id="container">
                     <div class="button">
@@ -31,16 +67,20 @@ include_once("./class/User.php")
                         <a href="./inscription.php">Crée compte</a>
                     </div>
                 </div>
-            <?php
-            } else if (isset($_SESSION)) { ?>
+                    <?php
+                    } else if (isset($_SESSION)) { ?>
                 <div id="container">
                     <div class="button">
                         <a href="./deconnexion.php">Se déconnecter</a>
                     </div>
                 </div>
             <?php } ?>
+                    <!--               fin de zone à revoir              -->
         </div>
     </main>
+    <footer>
+        <?php include_once('./include/footer_inc.php') ?>
+    </footer>
 </body>
 
 </html>
